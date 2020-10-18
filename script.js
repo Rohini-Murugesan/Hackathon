a1f78debe8b652ae159f5d8ae7e242dcf09c022d
Rohini <rohini.m@tataelxsi.co.in>
2020-10-18 14:52:34 +0530

    Restaurant page

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const APIKEY = "d562feeaf7ad62452461ee883ea94f51"
+const APIKEY = "fdbfcf88cb113edecaab164c46d51766" //"d562feeaf7ad62452461ee883ea94f51"
 var restDetails;
 var details;
 // * API 
@@ -14,8 +14,8 @@ async function getCollections() {
     return collections
 }
 
-async function getRestaurants(query) {
-    let url = "https://developers.zomato.com/api/v2.1/search?entity_id=7&entity_type=city&q="+query
+async function getRestaurantDetails(id) {
+    let url = "https://developers.zomato.com/api/v2.1/restaurant?res_id=" + id
     let apiData = await fetch(url, {
         headers: {
             'Content-type': 'application/json; charset=UTF-8',
@@ -23,7 +23,19 @@ async function getRestaurants(query) {
         }
     })
     let collections = await apiData.json()
-    console.log("getRestaurants",collections)
+    return collections
+}
+
+async function getRestaurants(query) {
+    let url = "https://developers.zomato.com/api/v2.1/search?entity_id=7&entity_type=city&q=" + query
+    let apiData = await fetch(url, {
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+            'user-key': APIKEY
+        }
+    })
+    let collections = await apiData.json()
+    // console.log("getRestaurants", collections)
     return collections
 }
 
@@ -36,7 +48,7 @@ async function getLocalities() {
         }
     })
     let collections = await apiData.json()
-    console.log(collections)
+    //  console.log(collections)
     return collections
 }
 
@@ -99,10 +111,10 @@ let generateCards = function (cardDetails) {
 }
 
 let generateCollections = (collectionsDetails, page = "mainPage") => {
-    console.log(collectionsDetails,page)
+    // console.log(collectionsDetails, page)
     let col = document.getElementById("collectionsContent")
-    console.log(col)
-    if(col!==null){
+    // console.log(col)
+    if (col !== null) {
         col.remove()
     }
     if (page === "mainPage") {
@@ -113,7 +125,42 @@ let generateCollections = (collectionsDetails, page = "mainPage") => {
         div.append(rowdiv)
         for (i = 0; i < 4; i++) {
             data = collectionsDetails.collections[i].collection
-            console.log(data)
+            // console.log(data)
+
+            coldiv = createElmAndSetAttr({
+                'class': 'custom-collection',
+                'href': 'collections.html',
+                'style': "text-decoration: none;"
+            }, "a")
+            coldiv.style.backgroundImage = "url(" + data.image_url + ")";
+            rowdiv.append(coldiv)
+
+            //title and count
+            s = createElmAndSetAttr({
+                "class": "text-light"
+            }, "span")
+            s.innerText = data.title
+            coldiv.append(s)
+            s = createElmAndSetAttr({
+                "class": "text-light p-2"
+            }, "span")
+            s.innerHTML = data.res_count + " places <i class='fas fa-caret-right'></i>"
+            coldiv.append(s)
+
+        }
+    } else if (page = "collections_page") {
+        document.getElementById("Handpicked").setAttribute("class", "text-danger p-3")
+        document.getElementById("Saved").setAttribute("class", "text-dark p-3")
+        div = document.getElementById("handpickedCollections")
+        rowdiv = createElmAndSetAttr({
+            'class': 'row mt-5',
+            'id': 'collectionsContent'
+        }, "div")
+        div.append(rowdiv)
+        // console.log(collectionsDetails.collections.length)
+        for (i = 0; i < collectionsDetails.collections.length; i++) {
+            data = collectionsDetails.collections[i].collection
+            // console.log(data)
 
             coldiv = createElmAndSetAttr({
                 'class': 'custom-collection',
@@ -136,46 +183,11 @@ let generateCollections = (collectionsDetails, page = "mainPage") => {
             coldiv.append(s)
 
         }
-    }else if(page="collections_page"){
-        document.getElementById("Handpicked").setAttribute("class","text-danger p-3")
-        document.getElementById("Saved").setAttribute("class","text-dark p-3")
-        div = document.getElementById("handpickedCollections")
-        rowdiv = createElmAndSetAttr({
-            'class': 'row mt-5',
-            'id' : 'collectionsContent'
-        }, "div")
-        div.append(rowdiv)
-        // console.log(collectionsDetails.collections.length)
-        for (i = 0; i < collectionsDetails.collections.length; i++) {
-            data = collectionsDetails.collections[i].collection
-            console.log(data)
-
-            coldiv = createElmAndSetAttr({
-                'class': 'custom-collection',
-                'href': '#',
-                'style': "text-decoration: none;"
-            }, "a")
-            coldiv.style.backgroundImage = "url(" + data.image_url + ")";
-            rowdiv.append(coldiv)
-
-            //title and count
-            s = createElmAndSetAttr({
-                "class": "text-light"
-            }, "span")
-            s.innerText = data.title
-            coldiv.append(s)
-            s = createElmAndSetAttr({
-                "class": "text-light p-2"
-            }, "span")
-            s.innerHTML = data.res_count + " places <i class='fas fa-caret-right'></i>"
-            coldiv.append(s)
-
     }
-}
 }
 
 let generateLocalities = (localitiesDetails) => {
-    console.log("generateLocalities", localitiesDetails)
+    // console.log("generateLocalities", localitiesDetails)
     div = document.getElementById("locality")
     rowdiv = createElmAndSetAttr({
         'class': 'row mt-5'
@@ -248,7 +260,7 @@ let main = () => {
         collectionsDetails = data
         generateCollections(collectionsDetails)
     }).catch((err) => {
-        console.log(err)
+        // console.log(err)
     })
 
     //Popular Localities
@@ -256,7 +268,7 @@ let main = () => {
         localitiesDetails = data
         generateLocalities(localitiesDetails)
     }).catch((err) => {
-        console.log(err)
+        // console.log(err)
     })
 }
 
@@ -264,46 +276,51 @@ let main = () => {
 
 
 // * Event Listeners
-document.getElementById("searchByLocation").addEventListener("click", ()=>{
-    console.log("hello")
+document.getElementById("searchByLocation").addEventListener("click", () => {
+    // console.log("hello")
     let id = document.getElementById("LocationDetails")
-    if(id!==null){
+    if (id !== null) {
         id.remove()
-    }else{
-        maindiv = document.getElementById("locationsOptions")//createElmAndSetAttr({'class':'container'},"div")
+    } else {
+        maindiv = document.getElementById("locationsOptions") //createElmAndSetAttr({'class':'container'},"div")
         rowdiv = createElmAndSetAttr({
             'class': 'row mt-1',
-            'style':'height:200px',
-            'id' : 'LocationDetails'
+            'style': 'height:200px',
+            'id': 'LocationDetails'
         }, "div")
         maindiv.append(rowdiv)
         coldiv = createElmAndSetAttr({
             'class': 'col-lg-4 offset-2 bg-white',
-            'id':'custom-dropdown'
+            'id': 'custom-dropdown'
         }, "div")
         rowdiv.append(coldiv)
-    
+
         div1 = createElmAndSetAttr({
-            'class':'text-danger font-weight-bold p-2 mt-2',
-            'onClick':'getCurrentLocation()'
+            'class': 'text-danger font-weight-bold p-2 mt-2',
+            'onClick': 'getCurrentLocation()'
         }, "div")
         div1.innerHTML = "<i class='fas fa-globe-americas'></i> Detect current location"
         coldiv.append(div1)
-        console.log("details",details)
-        popularLocations = Object.keys(details).slice(0,7)//['Velachery, Chennai','Porur, Chennai','T.Nagar, Chennai']
+        // console.log("details", details)
+        try {
+
+            popularLocations = Object.keys(details).slice(0, 7) //
+        } catch {
+            popularLocations = ['Velachery, Chennai', 'Porur, Chennai', 'T.Nagar, Chennai']
+        }
 
         div1 = createElmAndSetAttr({
-            'class':'dropdown-header p-2 mt-2',
-            'style':'color:grey'
+            'class': 'dropdown-header p-2 mt-2',
+            'style': 'color:grey'
         }, "div")
         div1.innerText = "Popular Locations"
         coldiv.append(div1)
-        for(i=0;i<popularLocations.length;i++){
+        for (i = 0; i < popularLocations.length; i++) {
             div1 = createElmAndSetAttr({
-                'class':'p-1 mt-2',
-                'style':'color:grey',
-                'id':popularLocations[i],
-                 'onClick':'searchLocation(this.id)'
+                'class': 'p-1 mt-2',
+                'style': 'color:grey',
+                'id': popularLocations[i],
+                'onClick': 'searchLocation(this.id)'
             }, "div")
             div1.innerText = popularLocations[i]
             coldiv.append(div1)
@@ -314,73 +331,73 @@ document.getElementById("searchByLocation").addEventListener("click", ()=>{
 
 
 // * Event Listeners
-document.getElementById("searchByRestaurant").addEventListener("click", ()=>{
-    console.log("hello")
+document.getElementById("searchByRestaurant").addEventListener("click", () => {
+    // console.log("hello")
     let id = document.getElementById("LocationDetails")
-    if(id!==null){
+    if (id !== null) {
         id.remove()
-    }else{
-        maindiv = document.getElementById("restaurantsOptions")//createElmAndSetAttr({'class':'container'},"div")
+    } else {
+        maindiv = document.getElementById("restaurantsOptions") //createElmAndSetAttr({'class':'container'},"div")
         rowdiv = createElmAndSetAttr({
             'class': 'row mt-1',
-            'style':'height:200px',
-            'id' : 'LocationDetails'
+            'style': 'height:200px',
+            'id': 'LocationDetails'
         }, "div")
         maindiv.append(rowdiv)
         coldiv = createElmAndSetAttr({
             'class': 'col-lg-5 offset-6 bg-white',
-            'id':'custom-dropdown-rest'
+            'id': 'custom-dropdown-rest'
         }, "div")
         rowdiv.append(coldiv)
-        
-        popularLocations = ['Velachery, Chennai','Porur, Chennai','T.Nagar, Chennai']
+
+        popularLocations = ['Velachery, Chennai', 'Porur, Chennai', 'T.Nagar, Chennai']
         query = document.getElementById("searchByLocation").value
         div1 = createElmAndSetAttr({
-            'class':'dropdown-header p-2 mt-2',
-            'style':'color:grey'
+            'class': 'dropdown-header p-2 mt-2',
+            'style': 'color:grey'
         }, "div")
-        div1.innerText = "Popular Restaurants in "+query
+        div1.innerText = "Popular Restaurants in " + query
         coldiv.append(div1)
         getRestaurants(query).then((data) => {
             restDetails = data
-            console.log("In func",restDetails)
+            // console.log("In func", restDetails)
             showRestaurants(restDetails)
         }).catch((err) => {
-            console.log(err)
+            // console.log(err)
         })
-
-
     }
 });
 
 
-let showRestaurants = ()=>{
-            for(i=0;i<5;i++){
-            data  = restDetails.restaurants[i].restaurant
-            div1 = createElmAndSetAttr({
-                'class':'p-1 mt-2 hover-this',
-                'style':'color:grey',
-                'id':data.name,
-                 'onClick':'openRestaurant(this.id)'
-            }, "div")
-            div1.innerText = data.name
-            coldiv.append(div1)
-        }
+let showRestaurants = () => {
+    for (i = 0; i < 5; i++) {
+        data = restDetails.restaurants[i].restaurant
+        div1 = createElmAndSetAttr({
+            'class': 'p-1 mt-2 hover-this',
+            'style': 'color:grey',
+            'id': data.id,
+            'onClick': 'getRestDetails(this.id)',
+            'data-toggle': 'modal',
+            'data-target': '#RestaurantModal'
+        }, "div")
+        div1.innerText = data.name
+        coldiv.append(div1)
+    }
 }
 
 // Location ------------------------------------------------------------------------------------------------------------------
-let getCurrentLocation = ()=>{
+let getCurrentLocation = () => {
     var loc = document.getElementById("searchByLocation");
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(getPosition);
-      } else { 
+    } else {
         loc.innerHTML = "Geolocation is not supported by this browser.";
-      } 
+    }
 }
 
 async function getPosition(position) {
-    console.log(position)
-    let url = "https://developers.zomato.com/api/v2.1/locations?query=India&lat="+position.coords.latitude+"&lon="+position.coords.longitude
+    // console.log(position)
+    let url = "https://developers.zomato.com/api/v2.1/locations?query=India&lat=" + position.coords.latitude + "&lon=" + position.coords.longitude
     let apiData = await fetch(url, {
         headers: {
             'Content-type': 'application/json; charset=UTF-8',
@@ -392,33 +409,33 @@ async function getPosition(position) {
     document.getElementById("LocationDetails").remove()
 }
 
-let searchLocation = (id)=>{
+let searchLocation = (id) => {
     document.getElementById("searchByLocation").value = id
     document.getElementById("LocationDetails").remove()
 }
 
 //---------------------------------------------------------------------------------------------------------------------------------
 
-let handpicked = ()=>{
+let handpicked = () => {
     getCollections().then((data) => {
         collectionsDetails = data
-        generateCollections(collectionsDetails,"collections_page")
+        generateCollections(collectionsDetails, "collections_page")
     }).catch((err) => {
-        console.log(err)
+        // console.log(err)
     })
 }
 
 
-let savedCollection = ()=>{
+let savedCollection = () => {
     let col = document.getElementById("collectionsContent")
-    console.log(col)
-    if(col!==null){
+    // console.log(col)
+    if (col !== null) {
         col.remove()
     }
     div = document.getElementById("savedCollections")
     rowdiv = createElmAndSetAttr({
         'class': 'row mt-5',
-        'id' : 'collectionsContent'
+        'id': 'collectionsContent'
     }, "div")
     div.append(rowdiv)
 
@@ -451,59 +468,101 @@ let savedCollection = ()=>{
     coldiv.append(s)
     rowdiv.append(coldiv)
 
-    document.getElementById("Handpicked").setAttribute("class","text-dark p-3")
-    document.getElementById("Saved").setAttribute("class","text-danger p-3")
+    document.getElementById("Handpicked").setAttribute("class", "text-dark p-3")
+    document.getElementById("Saved").setAttribute("class", "text-danger p-3")
 }
 
 
 
-let createZomatoFooter = ()=>{
+let createZomatoFooter = () => {
     div = document.getElementById("ZomatoFooter")
     div.innerHTML += '<div class="row"><div id="zomato-black" class="ml-5 mt-5">ZOMATO</div></div>'
     rowdiv = createElmAndSetAttr({
         'class': 'row mt-2',
-        'id' : 'footerContent'
+        'id': 'footerContent'
     }, "div")
     div.append(rowdiv)
-    let Details = {'COMPANY':['Who we are','Blog','Careers','Report Fraud','Contact'],
-                    'FOR FOODIES':['Code of Conduct','Community','Blogger Help','Developers','Mobile Apps'],
-                    'For Restaurant':['Add restaurant','Claim your Listing','Business App','Restaurant Widgets','Product for businesses'],
-                'For You':['Privacy','Terms','Security','SiteMap']}
-    for(i=0;i<Object.keys(Details).length;i++){
+    let Details = {
+        'COMPANY': ['Who we are', 'Blog', 'Careers', 'Report Fraud', 'Contact'],
+        'FOR FOODIES': ['Code of Conduct', 'Community', 'Blogger Help', 'Developers', 'Mobile Apps'],
+        'For Restaurant': ['Add restaurant', 'Claim your Listing', 'Business App', 'Restaurant Widgets', 'Product for businesses'],
+        'For You': ['Privacy', 'Terms', 'Security', 'SiteMap']
+    }
+    for (i = 0; i < Object.keys(Details).length; i++) {
         let key = Object.keys(Details)[i]
         coldiv = createElmAndSetAttr({
             'class': 'col-lg-3',
             'style': "text-decoration: none;"
         }, "div")
         rowdiv.append(coldiv)
-    
+
         ul = createElmAndSetAttr({
             class: "font-weight-bold m-2",
-            style:"list-style-type:none;"
+            style: "list-style-type:none;"
         }, "ul")
         ul.innerText = key.toUpperCase()
         coldiv.append(ul)
-        
-        for(j=0;j<Details[key].length;j++){
-            li = createElmAndSetAttr({
-            }, "li")
+
+        for (j = 0; j < Details[key].length; j++) {
+            li = createElmAndSetAttr({}, "li")
             a = createElmAndSetAttr({
-                "href":"#",
-                "class" : "text-dark font-weight-light p-3",
+                "href": "#",
+                "class": "text-dark font-weight-light p-3",
                 "style": "text-decoration: none;"
             }, "a")
             a.innerText = Details[key][j]
             li.append(a)
             ul.append(li)
         }
-    }    
-    hr = createElmAndSetAttr({
-    }, "hr")
+    }
+    hr = createElmAndSetAttr({}, "hr")
     div.append(hr)
 
-    p = createElmAndSetAttr({
-    }, "p")
+    p = createElmAndSetAttr({}, "p")
     p.innerHTML = "<small>By continuing past this page, you agree to our Terms of Service, Cookie Policy, Privacy Policy and Content Policies. All trademarks are properties of their respective owners. 2008-2020 © Zomato™ Pvt Ltd. All rights reserved.</small>"
     div.append(p)
-    
+}
+
+
+let getRestDetails = (id) => {
+    // console.log(id)
+    getRestaurantDetails(id).then((data) => {
+        restDetails = data
+        // console.log(restDetails)
+        openRestaurant(restDetails)
+    }).catch((err) => {
+        // console.log(err)
+    })
+}
+
+
+let openRestaurant = (restDetails) => {
+    // console.log(restDetails)
+    document.getElementById("modalRestName").innerText = restDetails.name
+    document.getElementById("modalRestImage").href = restDetails.photos_url
+    document.getElementById("modalImg2").src = restDetails.featured_image
+    document.getElementById("modalRestMenu").href = restDetails.menu_url
+    document.getElementById("modalSpan1").innerText = restDetails.location.address
+    document.getElementById("modalP1").innerText = "  " + restDetails.phone_numbers
+    document.getElementById("modalCost").innerText = "  " + restDetails.average_cost_for_two + " Avg cost for two"
+
+    modal = document.getElementById("modalhighlights")
+    for (i = 0; i < restDetails.highlights.length; i++) {
+        div = createElmAndSetAttr({
+            "class": "btn btn-outline-info p-1 m-2"
+        }, "span")
+        div.innerText = restDetails.highlights[i]
+        modal.append(div)
+    }
+
+    modal = document.getElementById("modalCuisines")
+    let cuisines = restDetails.cuisines.split(",")
+    for (i = 0; i < cuisines.length; i++) {
+        div = createElmAndSetAttr({
+            "class": "btn btn-outline-success p-1 m-2"
+        }, "span")
+        div.innerText = cuisines[i]
+        modal.append(div)
+    }
+    return restDetails
 }
